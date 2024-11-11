37ac49a4d8032f52b50f9389476f97c21cf82d13
KrynStackk <letruongthinh1712@gmail.com>
2024-11-11 08:41:15 +0700

    Update segformer_head.py, add ASPP sau concat, get inputs[2][3]

--- mmseg/models/decode_heads/segformer_head.py
+++ mmseg/models/decode_heads/segformer_head.py
@@ -105,4 +105,4 @@ class SegformerHead(BaseDecodeHead):
         out = self.aspp(out)
         # out = self.cls_seg(out)
 
-        return out, intputs[2], inputs[3]
+        return out, inputs[2], inputs[3]
